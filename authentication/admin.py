1241ce060dc8be28b1ee8926aaad5627a46cf423
Galaluddin Owais <galaluddin.o@bronto24.com>
2025-09-23 17:22:49 +0300

    add welcome template

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'apple_user_id', 'created_at']
-    search_fields = ['user__email', 'apple_user_id']
