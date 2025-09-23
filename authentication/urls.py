53868987485ea325f08bd0ee4d095e4394c99a8f
Galaluddin Owais <galaluddin.o@bronto24.com>
2025-09-23 17:26:20 +0300

    fix: remove url

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.user_info, name='user_info'),
     path('logout/', views.logout_view, name='logout'),
-    path('login-success/', views.login_success, name='login_success'),
     path('login-error/', views.login_error, name='login_error'),
     path('welcome/', views.welcome_view, name='welcome'),
 ]
